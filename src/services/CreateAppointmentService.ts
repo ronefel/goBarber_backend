e3a4119a94d4770bbc6159d68445f4e394207ce3
ronefel <ronefel@hotmail.com>
2020-08-25 13:39:13 -0400

    initial commit with database configured

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,34 @@
+import { startOfHour } from 'date-fns';
+
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    if (await appointmentsRepository.findByDate(appointmentDate)) {
+      throw new Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
